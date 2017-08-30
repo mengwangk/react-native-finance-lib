79a68537a87b2d2996e7c27ea600d39d0d328c25
mengwangk <mengwangk@gmail.com>
2017-08-31 00:31:39 +0800

    Modified files

---
+++ android/src/test/java/com/myinvestor/finance/MyInvestorFinanceTest.java
@@ -0,0 +1,15 @@
+package com.myinvestor.finance;
+
+import org.junit.Test;
+
+/**
+ * Unit testing for the finance APIs.
+ */
+
+public class MyInvestorFinanceTest {
+
+    @Test
+    public void testGetStock(){
+
+    }
+}
